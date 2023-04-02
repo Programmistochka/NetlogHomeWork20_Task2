18581afd278513373e693d37d21c1a40c089f0d0
Programmistochka <programmistochka@list.ru>
2023-04-02 13:55:36 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+import datetime
+
+
+def logger(path):
+    
+    file_path = path
+    
+    def __logger(old_function):
+        def new_function(*args, **kwargs):
+            with open(file_path, 'a') as file: 
+                file.write(f'Start date and time: {datetime.datetime.now()}\n')
+                file.write(f'Name function: {old_function}\n')
+                file.write(f'Positional parametrs: {args}\n')
+                file.write(f'Named parametrs: {kwargs}\n')
+                rez = old_function(*args, **kwargs)
+                file.write(f'Result: {rez}\n\n')
+            return rez
+
+        return new_function
+
+    return __logger
+
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_2()
